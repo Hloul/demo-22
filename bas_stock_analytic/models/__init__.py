8a9ea8e092897391a8e3ba5c06963639ddd01233
Wafi Chaar <wafi.chaar@bas.sarl>
2022-12-05 00:16:37 +0200

    remove unneeded files

--- bas_stock_analytic/models/__init__.py
+++ bas_stock_analytic/models/__init__.py
@@ -5,7 +5,6 @@
 from . import purchase_order_line
 from . import sale_order_line
 from . import stock_move
-from . import stock_picking
 from . import stock_rule
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
