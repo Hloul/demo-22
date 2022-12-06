ed1722fe2f99369e644f15dc4f78fbd52b041b00
Wafi Chaar <wafi.chaar@bas.sarl>
2022-12-06 07:36:54 +0200

    add logging

--- bas_stock_analytic/models/sale_order_line.py
+++ bas_stock_analytic/models/sale_order_line.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of BAS. See LICENSE file for full copyright and licensing details.
+
+import logging
 from odoo import fields, models, _
 from odoo.exceptions import UserError
 from dateutil.relativedelta import relativedelta
@@ -7,6 +9,7 @@ from odoo.tools.float_utils import float_is_zero, float_compare
 from datetime import datetime
 from collections import namedtuple, OrderedDict, defaultdict
 
+_logger = logging.getLogger(__name__)
 
 
 
@@ -18,9 +21,13 @@ class SaleOrderLine(models.Model):
 
     def _prepare_procurement_values(self,group_id):
         res = super(SaleOrderLine,self)._prepare_procurement_values(group_id=group_id)
+        _logger.info('bas: self %s',self)
+        _logger.info('bas: analytic distribution %s',self.analytic_distribution)
+
         res.update({
                 'analytic_distribution': self.analytic_distribution,
             })
+        _logger.info('bas: res %s',res)
         return res
     
 
